d6c57d31df13577c86fe11fc192a70c6d09efddd
Neylanio <neylaniogomes@gmail.com>
2020-08-05 17:39:27 -0300

    feat(web): Create TeacherList page and PageHeader component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
 
 const TeacherForm: React.FC = () => {
   return (
-    <h1>Teacher Form</h1>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas.">
+        teste
+      </PageHeader>
+    </div>
   );
 }
 
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logoImg from '../../assets/images/icons/logo.svg';
-import backIcon from '../../assets/images/icons/back.svg';
 
-// import { Container } from './styles';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
 
 const TeacherList: React.FC = () => {
   return (
     <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Voltar" />
-          </Link>
-          <img src="" alt="" />
-        </div>
-      </header>
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week-day">Dia da semana</label>
+            <input type="text" id="week_day"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time"/>
+          </div>
+        </form>
+      </PageHeader>
     </div>
   );
 }
