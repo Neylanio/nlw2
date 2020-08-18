8e01e0ca681c1d0e13a0eb6628b2c8bb3e7fe39c
Neylanio <neylaniogomes@gmail.com>
2020-08-18 14:23:01 -0300

    feat(mobile): Create Select Input week_days

--- mobile/src/pages/TeacherLIst/index.tsx
+++ mobile/src/pages/TeacherLIst/index.tsx
@@ -1,15 +1,65 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Picker } from 'react-native';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
-import { ScrollView } from 'react-native-gesture-handler';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
 const TeacherLIst: React.FC = () => {
+
+  const [ selectedDay, setSelectedDay ] = useState('');
+  const [ selectedTime, setSelectedTime ] = useState('');
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis"/>
+      <PageHeader title="Proffys disponíveis">
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput 
+            style={styles.input} 
+            placeholder="Qual a matéria?"
+            placeholderTextColor= "#c1bccc"
+          />
+
+          <View style={styles.inputGroup}>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da semana</Text>
+              <View style={styles.inputSelect}>
+                <Picker
+                  mode= "dialog"
+                  selectedValue={selectedDay}
+                  onValueChange={(itemValue, itemIndex) => setSelectedDay(itemValue)}
+                >
+                  <Picker.Item label="Selecione o dia" value="99" />
+                  <Picker.Item label="Domingo" value="0" />
+                  <Picker.Item label="Seguna-feira" value="1" />
+                  <Picker.Item label="Terça-feira" value="2" />
+                  <Picker.Item label="Quarta-feira" value="3" />
+                  <Picker.Item label="Quinta-feira" value="4" />
+                  <Picker.Item label="Sexta-feira" value="5" />
+                  <Picker.Item label="Sábado" value="6" />
+                </Picker>
+              </View>
+              
+              {/* <TextInput 
+                style={styles.input} 
+                placeholder="Qual o dia?"
+                placeholderTextColor= "#c1bccc"
+              /> */}
+            </View>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput 
+                style={styles.input} 
+                placeholder="Qual horário?"
+                placeholderTextColor= "#c1bccc"
+              />
+            </View>
+          </View>
+        </View>
+
+      </PageHeader>
       
       <ScrollView
         style={styles.teacherList}
